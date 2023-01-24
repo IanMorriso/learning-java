a82e90026d86310d6ceb7bc540a8a3cbdc9e8455
IanMorriso <112591354+IanMorriso@users.noreply.github.com>
2023-01-23 21:32:36 -0700

    Added loops and conditionals

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        // Conditions and loops
+        if (20 > 18) {
+            System.out.println("20 is greater than 18");
+        }
+        
+        int time = 20;
+        String result = (time < 18) ? "Good day" : "Good evening";
+        System.out.println(result);
+
+        String[] cars = {"Volvo", "Ford", "BMW", "Mercury"};
+        for (String i : cars) {
+            System.out.println(i);
+        }
     }
 }
